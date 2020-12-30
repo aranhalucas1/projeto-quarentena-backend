f2badecc65eb4b56bab6393d1c18573616fce5dd
Lucas Aranha <21315381@anhembimorumbi.edu.br>
2020-12-30 17:23:06 -0300

    inicio do projeto

---
+++ src/main/java/com/projeto/quarentena/FiatluxApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.quarentena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiatluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiatluxApplication.class, args);
+	}
+
+}
